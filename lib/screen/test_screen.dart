381f08ca3e71b2aabc55fc81cfa5c7439bbbb935
Animesh <animesh@hestabit.in>
2022-04-18 14:17:30 +0530

    Sumamry: extension code  seperated from the screen - find_or_put_extension.dart: Move the extension code on seperated file - test_screen.dart: Include the extension file

--- lib/screen/test_screen.dart
+++ lib/screen/test_screen.dart
@@ -1,20 +1,7 @@
 import 'package:extension_testing/controller/test_controller.dart';
+import 'package:extension_testing/screen/find_or_put_extension.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
-extension FindOrPut on GetInterface {
-  S findOrPut<S>(
-    S dependency, {
-    String? tag,
-    bool permanent = false,
-    InstanceBuilderCallback<S>? builder,
-  }) {
-    if (GetInstance().isRegistered<S>(tag: tag)) {
-      return GetInstance().find<S>(tag: tag);
-    }
-    return GetInstance().put<S>(dependency, tag: tag, permanent: permanent);
-  }
-}
 
 class ExtensionTesting extends StatelessWidget {
   ExtensionTesting({Key? key}) : super(key: key);
